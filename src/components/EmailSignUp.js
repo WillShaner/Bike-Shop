4c5435308a43a26dd312bf859b241b76342c7d1a
William Shaner <williamshaner3@gmail.com>
2023-01-22 14:27:02 -0600

    styling for email popup

--- src/components/EmailSignUp.js
+++ src/components/EmailSignUp.js
@@ -4,6 +4,8 @@ import Button from 'react-bootstrap/Button';
 function EmailSignUp({ setEmailPopup }) {
   const handleSubmit = (ev) => {
     ev.preventDefault();
+    setEmailPopup(false);
+    document.body.classList.remove('overflow-hidden');
   };
 
   const closePopUp = () => {
@@ -18,8 +20,10 @@ function EmailSignUp({ setEmailPopup }) {
         <h2 className="text-center">STAY IN THE KNOW</h2>
         <p>Stay up to date on the latest, get special member discounts, and lots of inspiration</p>
         <form className="d-flex align-items-center justify-content-center" onSubmit={handleSubmit}>
-          <input className="ps-3 py-1 rounded" type="email" placeholder="email address" />
-          <Button type="submit">Sign up</Button>
+          <div className="input-row">
+            <input className="ps-3 py-1 rounded" type="email" placeholder="email address" required />
+            <Button type="submit">Sign up</Button>
+          </div>
         </form>
         <p className="close-email-signup" onClick={() => closePopUp()} aria-hidden>No thanks</p>
       </div>
